cb3b6a9947d07b66c4d331fd8958cdcd0b0c11c0
Silveira11 <108001333+Silveira11@users.noreply.github.com>
2025-10-24 23:53:55 -0300

    Adicionado Exercicio5

---
+++ src/Exercicio5.java
@@ -0,0 +1,28 @@
+public class Exercicio5 {
+        public static void main(String[] args) {
+            String texto = "Hello World!";
+
+            String textoInvertido = inverterString(texto);
+
+            System.out.println("String original: " + texto);
+            System.out.println("String invertida: " + textoInvertido);
+        }
+
+        public static String inverterString(String texto) {
+            char[] caracteres = texto.toCharArray();
+            int inicio = 0;
+            int fim = caracteres.length - 1;
+
+            while (inicio < fim) {
+                char temp = caracteres[inicio];
+                caracteres[inicio] = caracteres[fim];
+                caracteres[fim] = temp;
+
+                inicio++;
+                fim--;
+            }
+
+            return new String(caracteres);
+        }
+    }
+
