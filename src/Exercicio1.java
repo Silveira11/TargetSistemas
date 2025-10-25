41053e266bd2808b11bbcc9722b8a807cb7ad71e
Silveira11 <108001333+Silveira11@users.noreply.github.com>
2025-10-24 22:59:48 -0300

    Initial commit

---
+++ src/Exercicio1.java
@@ -0,0 +1,15 @@
+import java.sql.SQLOutput;
+
+public class Exercicio1 {
+
+    public static void main(String[] args) {
+        int Indice = 13;
+        int Soma = 0;
+        int K = 0;
+        while (K < Indice){
+            K = K+1; Soma = Soma + K;
+        }
+        System.out.println(Soma);
+
+    }
+}
